ff76657128fbf2f1d292b020f3dbeb154573a5d7
martinezguilherme <guilherme.martinez.1313@gmail.com>
2021-08-14 00:57:12 -0300

    Dano por flecha implementado!

---
+++ Projeto final - jogo/Assets/Codigos/danoFlecha.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class danoFlecha : MonoBehaviour
+{
+
+    public LayerMask camadaInimigos;
+    public int danoBaseFlecha = 15;
+    float raioDanoFlecha = 0.25f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Collider[] inimigosAcertados = Physics.OverlapSphere(transform.position, raioDanoFlecha, camadaInimigos);
+
+        foreach (Collider inimigo in inimigosAcertados)
+        {
+            Debug.Log("Acertamos com flecha: " + inimigo.name);
+            inimigo.GetComponent<inimigo>().receberDano(danoBaseFlecha);
+
+            // Destroi a flecha após acertar um inimigo
+            DestroyImmediate(gameObject);
+        }
+
+    }
+}
